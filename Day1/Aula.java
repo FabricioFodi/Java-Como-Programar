83242ea3ab03687dbd8e5c661efd21810a82ca5d
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-06-10 15:18:59 -0300

    Day 1

---
+++ Day1/Aula.java
@@ -0,0 +1,16 @@
+package Day1;
+public class Aula {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Aula 1 - Variáveis");
+        System.out.println("A soma de 'a' + 'b' é: " + (a + b));
+        System.out.println("A subtração de 'a' - 'b' é: " + (a - b));
+        System.out.println("A multiplicação de 'a' * 'b' é: " + (a * b));
+        System.out.println("A divisão de 'a' / 'b' é: " + (a / b));
+        System.out.println("O resto da divisão de 'a' % 'b' é: " + (a % b));
+        System.out.println("A raiz quadrada de 'a' é: " + Math.sqrt(a)); // Math é uma classe da bilbioteca Java para operações matemáticas
+        System.out.println("A raiz quadrada de 'b' é: " + Math.sqrt(b)); // sqrt() é um método da class Math que calcula a raiz quadrada. sqrt() significa Square Root (raiz quadrada)
+    }
+}
