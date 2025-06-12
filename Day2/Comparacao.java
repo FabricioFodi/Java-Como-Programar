bcac048525a96d48050f7b1c8f0409acc85e5aaf
Fabricio Fodi <fabricio_fodi@estudante.sc.senai.br>
2025-06-12 17:12:46 -0300

    Day 2 commit

---
+++ Day2/Comparacao.java
@@ -0,0 +1,7 @@
+package Day2;
+// Figura 2.15: Comparacao.java
+// Compara inteiros utilizando instruções if, operadores relacionais
+// e operadores de igualdade.
+public class Comparacao {
+    
+}
